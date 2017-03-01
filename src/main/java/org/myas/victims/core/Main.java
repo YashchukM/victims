b7d7715c77e23a9e8439c0f4b5caff54dd7ad428
Mykhailo Yashchuk <19mihail@gmail.com>
2017-03-01 10:14:14 +0200

    Adds Apache Tika test

--- src/main/java/org/myas/victims/core/Main.java
+++ src/main/java/org/myas/victims/core/Main.java
@@ -1,7 +1,35 @@
 package org.myas.victims.core;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
 /**
  * Created by Mykhailo Yashchuk on 28.02.2017.
  */
 public class Main {
+    public static void main(String[] args) throws IOException, TikaException, SAXException {
+        testParse();
+    }
+
+    private static void testParse() throws IOException, TikaException, SAXException {
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        FileInputStream inputStream = new FileInputStream(new File("holodomor.pdf"));
+        ParseContext parseContext = new ParseContext();
+
+        PDFParser parser = new PDFParser();
+        parser.parse(inputStream, handler, metadata, parseContext);
+
+        for (String name: metadata.names()) {
+            System.out.println(name + ": " + metadata.get(name));
+        }
+    }
 }
